52c73e23494d6a24a2df7e6278495799c5712920
degawl <devongawley@gmail.com>
2018-10-24 19:08:45 +0300

    Firebase email auth initial setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+                apiKey: 'AIzaSyAHyEo0K8y5qtMPzqjdoFN6-4EE_BY0y3I',
+                authDomain: 'authapp-cd849.firebaseapp.com',
+                databaseURL: 'https://authapp-cd849.firebaseio.com',
+                projectId: 'authapp-cd849',
+                storageBucket: 'authapp-cd849.appspot.com',
+                messagingSenderId: '244392643032'
+        });
+    }
+    
     render() {
         return (
             <View>
